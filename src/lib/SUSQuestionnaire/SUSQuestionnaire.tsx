9b26390ecb40db3a485f04a1902b1eacc5e07378
Viktor <viktor@intrykk.se>
2020-05-08 11:01:01 +0200

    Update description

--- src/lib/SUSQuestionnaire/SUSQuestionnaire.tsx
+++ src/lib/SUSQuestionnaire/SUSQuestionnaire.tsx
@@ -93,8 +93,8 @@ export const SUSQuestionnaire = ({ callback }: SUSQuestionnaireProps) => {
       <Text type="header">System Usability Scale</Text>
 
       <Text type="onGrayBackground" margin="bottom+horizontal">
-        Dessa uppgifter samlas in för att säkerställa att vi får en varierad
-        grupp personer som besvarar undersökningen.
+        System Usability Scale används för att mäta en användares upplevelse av
+        en teknisk produkt. Alla frågor är obligatoriska.
       </Text>
 
       <View padding="horizontal" borderTopBottom={true}>
